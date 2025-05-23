6a32fe84b0464643ed0b9117ae2bde819a8bc93f
Dagonin <dagonins@gmail.com>
2025-05-23 15:33:11 +0200

    1

--- app/src/main/java/com/example/mobilki/ui/VideoPlayer.kt
+++ app/src/main/java/com/example/mobilki/ui/VideoPlayer.kt
@@ -2,14 +2,17 @@ package com.example.mobilki.ui
 
 import android.net.Uri
 import android.view.ViewGroup
+import androidx.annotation.OptIn
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.viewinterop.AndroidView
+import androidx.media3.common.util.UnstableApi
 import androidx.media3.common.MediaItem as ExoMediaItem
 import androidx.media3.exoplayer.ExoPlayer
 import androidx.media3.ui.PlayerView
 
+@OptIn(UnstableApi::class)
 @Composable
 fun VideoPlayer(
     uri: Uri,
